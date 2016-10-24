1ee23b594c5988dc3ab46110a106c938fd1f1456
shakadak <nathanael.pineau@gmail.com>
2016-10-24 23:01:18 +0200

    some more

---
+++ chips.js
@@ -0,0 +1,36 @@
+global chipType =	[ "attack"	: [1, 19, 18, 2, 7, 6, 5, 30, 33, 32, 31, 36, 85, 105]
+					, "heal"	: [3, 4, 10, 11, 89, 35, 67, 80, 90, 84]
+					, "shield"	: [21, 23, 20, 24, 22, 29, 81]
+					, "buff"	: [9, 8, 15, 14, 25, 12, 16, 88, 96, 28, 26, 91, 102, 13, 103, 17, 104]
+					, "debuff"	: [92, 94, 106, 93, 95]
+					, "bulb"	: [73, 76, 77, 75, 79, 74, 78]
+					, "poison"	: [97, 98, 99]
+					, "mirror"	: [100, 101]
+					, "tactic"	: [34, 110, 59, 68] ];
+
+global cAttaques =
+[1 : true, 19 : true, 18 : true, 2 : true, 7 : true, 6 : true, 5 : true, 30 : true, 33 : true, 32 : true, 31 : true, 36 : true, 85 : true, 105 : true];
+
+global cSoins =
+[3 : true, 4 : true, 10 : true, 11 : true, 89 : true, 35 : true, 67 : true, 80 : true, 90 : true, 84 : true];
+
+global cProtections =
+[21 : true, 23 : true, 20 : true, 24 : true, 22 : true, 29 : true, 81 : true];
+
+global cBoosts =
+[9 : true, 8 : true, 15 : true, 14 : true, 25 : true, 12 : true, 16 : true, 88 : true, 96 : true, 28 : true, 26 : true, 91 : true, 102 : true, 13 : true, 103 : true, 17 : true, 104 : true];
+
+global cBulbes =
+[73 : true, 76 : true, 77 : true, 75 : true, 79 : true, 74 : true, 78 : true];
+
+global cDebuffs =
+[92 : true, 94 : true, 106 : true, 93 : true, 95 : true];
+
+global cTactique =
+[34 : true, 110 : true, 59 : true, 68 : true];
+
+global cPoisons =
+[97 : true, 98 : true, 99 : true];
+
+global cMiroirs =
+[100 : true, 101 : true];
---
+++ main.js
@@ -0,0 +1,213 @@
+include("functional.js");
+include("bool.js");
+include("list.js");
+include("map");
+include("item");
+include("math.js");
+include("chips");
+include("game-state.js");
+include("pq.js");
+
+global totalPop;
+totalPop =@ 0;
+main();
+
+function getLaserTargets(@item, @caster) {
+	var center = caster['POS'];
+	var cx = getCellX(center);
+	var cy = getCellY(center);
+	var minr = getWeaponMinRange(item);
+	var ret =@ [];
+	var tgt = getCellFromXY(cx + minr, cy);
+	if (lineOfSight(center, tgt)) {
+		push(ret, tgt);
+	}
+	tgt =@ getCellFromXY(cx - minr, cy);
+	if (lineOfSight(center, tgt)) {
+		push(ret, tgt);
+	}
+	tgt =@ getCellFromXY(cx, cy + minr);
+	if (lineOfSight(center, tgt)) {
+		push(ret, tgt);
+	}
+	tgt =@ getCellFromXY(cx, cy - minr);
+	if (lineOfSight(center, tgt)) {
+		push(ret, tgt);
+	}
+	return ret;
+}
+
+function getRandomTarget(@state, @item) {
+	var area = getItemArea(item);
+	var validCells;
+	if (area == AREA_LASER_LINE) {
+		validCells =@ getLaserTargets(item, getSelf(state));
+	}
+	else if (item === CHIP_TELEPORTATION) {
+		validCells =@ getCellsToUseChipOnCell(CHIP_TELEPORTATION, getSelf(state)['POS']);
+	}
+	else {
+		var states =@ state["all"];
+		var validTargets =@ (isWeapon(item) || cAttaques[item]	? state["enemies"]
+							: item === CHIP_LIBERATION			? state['allies'] + state['enemies']
+							: state['allies']);
+		validCells =@ arrayFlatten(aMap(function(@x){return states[x]["A"+area];})(validTargets));
+	}
+	shuffle(validCells);
+	var target;
+	do {
+		target = shift(validCells);
+	} while (  target !== null
+				&& !canTargetCell(item)(getSelf(state)["POS"])(target));
+	return target;
+}
+
+function getMove(@gameState) {
+	if (getSelf(gameState)["MP"] === 0) { return []; }
+	var moveList =@ getMovementListFromState(gameState);
+	return aMap(delay(function(@cell) {
+		var moved = moveList["mcosts"][cell];
+		updateState(getSelf(gameState)["MP"])(subTo(moved));
+		updateState(getSelf(gameState)["POS"])(const(cell));
+		updateState(getSelf(gameState)["A1"])(const(getApplicableArea(AREA_POINT)(cell)));
+		updateState(getSelf(gameState)["A3"])(const(getApplicableArea(AREA_CIRCLE_1)(cell)));
+		updateState(getSelf(gameState)["A4"])(const(getApplicableArea(AREA_CIRCLE_2)(cell)));
+		updateState(getSelf(gameState)["A5"])(const(getApplicableArea(AREA_CIRCLE_3)(cell)));
+		return [delay(compose(moveTowardCell)(function(x){
+			//mark(x, COLOR_YELLOW);
+			//debug("M -> " + x);
+			return x;
+		}))(cell)];
+	}))(moveList["moves"]);
+}
+
+function getAction(@gameState) {
+	var itemList =@ getActionListFromState(gameState);
+	return aMap(delay(function(@item) {
+		var target = getRandomTarget(gameState, item);
+		if (target === null) { return void; }
+		var ret = [];
+		var cost = getItemCost(item);
+		if (isWeapon(item) && item !== gameState["equipped"]) {
+			cost += 1;
+			push(ret, delay(setWeapon)(item));
+			gameState["equipped"] = item;
+		}
+		updateState(getSelf(gameState)["TP"])(subTo(cost));
+		aIter(ITEMS_EFFECT[item](gameState, getSelf(gameState), target))(gameState["all"]);
+		if (isChip(item) && getChipCooldown(item) !== 0) { removeElement(gameState["chips"], item); }
+		push(ret, delay(compose(useItemOnCell(item))(function(x){
+				//mark(x, BEST_COLOR);
+				//debug(getItemName(item) + " -> " + target);
+				return x;
+		}))(target));
+		return ret;
+	}))(itemList);
+}
+
+function getActions(@gameState) {
+	var clonedState = gameState;
+	var actions = [];
+	for(var i = 0; i < 15; i++) {
+		var list = getMove(clonedState) + getAction(clonedState);
+		if (list === []) { break; }
+		actions += list[randInt(0, count(list))]();
+	}
+	return ["actions": actions, "state": clonedState];
+}
+
+function sumState(@all, @ls, @stat) {
+	return arrayFoldLeft(ls, function(@tlife, @leek) {
+			return tlife + all[leek][stat];
+	}, 0);
+}
+
+function applyEffects(@wearer) {
+	aIter(applyEffect(wearer))(wearer['EFFS']);
+}
+
+function evaluateState(o) {
+	var os =@ o["all"];
+	aIter(applyEffects)(os);
+	var oeLifes = sumState(os, o["enemies"], "HP");
+	var oaLifes = sumState(os, o["allies"], "HP");
+	var oASH = 1 + sumState(os, o['allies'], 'ASH');
+	var oRSH = 1 + sumState(os, o['allies'], 'RSH');
+	var oSTR = sumState(os, o['allies'], 'STR');
+	var oeTTP = sumState(os, o["enemies"], "TTP");
+	var oeTMP = sumState(os, o["enemies"], "TMP");
+	var oaTTP = sumState(os, o["allies"], "TTP");
+	var oaTMP = sumState(os, o["allies"], "TMP");
+	return memo1(function(@x) {
+		var xs =@ x["all"];
+		aIter(applyEffects)(xs);
+		var xeLifes = sumState(xs, x["enemies"], "HP");
+		var xaLifes = sumState(xs, x["allies"], "HP");
+		var xeTTP = sumState(xs, x["enemies"], "TTP");
+		var xeTMP = sumState(xs, x["enemies"], "TMP");
+		var xaTTP = sumState(xs, x["allies"], "TTP");
+		var xaTMP = sumState(xs, x["allies"], "TMP");
+		var xASH = 1 + sumState(xs, x['allies'], 'ASH');
+		var xRSH = 1 + sumState(xs, x['allies'], 'RSH');
+		var xSTR = sumState(xs, x['allies'], 'STR');
+		var getDist = function(@e) {
+			return getPathLength(xs[GLOBAL_LEEK_ID]["POS"], xs[e]["POS"]);
+			};
+		var xDist = arrayMin(aMap(getDist)(x["enemies"]));
+		xDist =@ max(1, (10000 + max(8, abs(xs[GLOBAL_LEEK_ID]["TMP"] - xDist))) / 10000);
+		return xDist
+			 * (xeLifes / oeLifes)
+			 * (oaLifes / xaLifes)
+			 * (oASH / xASH)
+			 * (oRSH / xRSH)
+			 * (oSTR / xSTR)
+			 * (xeTTP / oeTTP)
+			 * (xeTMP / oeTMP)
+			 * (oaTTP / xaTTP)
+			 * (oaTMP / xaTMP);
+	});
+}
+
+function main() {
+
+	if (getWeapon() === null) {
+		setWeapon(arraySort(getWeapons(), function(@x, @y) {
+			var xr = getWeaponMaxRange(x);
+			var yr = getWeaponMaxRange(y);
+			return xr > yr ? -1 : xr === yr ? 0 : 1;
+		})[0]);
+	}
+	initObstacles(aMap(getCell)(getAliveAllies() + getAliveEnemies()));
+	var gameState = getGameState();
+
+	var enemies = aFilter(negate(isSummon))(getAliveEnemies());
+	var moves = getlAccessibleCells(getMP())(getCell());
+	var simpleMoves = getKeys(moves);
+	var safeAccessibleCells =@ lgetSomewhatSafeCells(getDangerousItems)(simpleMoves)(enemies);
+	var checkSafeCell = bmemo(curry2(inArray)(safeAccessibleCells));
+
+	debug(getOperations() + 'op');
+
+	var evaluate = evaluateState(gameState);
+	var population = pqEmpty();
+	var popInsert = pqInsert(population);
+	var popSelect = pqPop(population);
+	var existing = [];
+
+	var safeMod = 1 - 0.15 * (32 - getTurn()) / 64;
+
+	while (getOperations() < 19000000) {
+		var actions =@ getActions(gameState);
+		var value = evaluate(actions["state"]);
+		if (checkSafeCell(actions['state']['all'][GLOBAL_LEEK_ID]['POS'])) {
+			value *= safeMod;
+		}
+	//	if (existing["" + actions["state"]])	{ continue; }
+	//	else						{ existing["" + actions["state"]] = true; }
+		totalPop++;
+		popInsert(value, actions["actions"]);
+	}
+	aIter(yield)(popSelect());
+}
+debug(getOperations() + 'op');
+debug("totalPop: " + totalPop);
