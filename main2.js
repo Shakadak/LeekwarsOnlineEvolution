1bb1a09cba5def069c77b20f9c0d5d8f55f21258
shakadak <nathanael.pineau@gmail.com>
2016-12-09 11:42:20 +0100

    no AoE QQ

--- main2.js
+++ main2.js
@@ -10,6 +10,7 @@ include("game-state.js");
 include("pq.js");
 include('Funno');
 include('danger');
+include('struct.js');
 
 global opin_move = 0, opout_move = 0;
 global opin_attack = 0, opout_attack = 0;
@@ -20,7 +21,11 @@ totalPop =@ 0;
 
 global ACTIONS_QUEUE = [];
 
+global ACTION = 0, STATE = 1, GENERATOR = 2;
+
 main();
+
+function act(@x) { var a; x(a, 0, 0); aIter(yield)(a); }
 
 function getTargets(@state, @item) {
 	var area = getItemArea(item);
@@ -85,7 +90,7 @@ function getAction(@gameState) {
 				if (cBulbes[item]) {
 					push(ret, function() {
 							summon(item, target, function() {
-									aIter(yield)(shift(ACTIONS_QUEUE));
+									aIter(act)(shift(ACTIONS_QUEUE));
 								});
 						});
 				}
@@ -102,40 +107,125 @@ function getAction(@gameState) {
 	}))(itemList);
 }
 
-function getActions(@gameState) {
-	var ret = ["actions": [], "state": gameState];
-	var clonedState =@ ret["state"];
-	var actions_queue =@ ret["actions"];
-	do {
-		var actions =@ (actions_queue[count(actions_queue)] = []);
-		//for(var i = 0; i < 15; i++) {
-		while (true) {
-			var breakout = false;
+function mutateActions(@actions, @baseState) {
+	var a = min(0, count(actions) - 1);
+	var k = randInt(0, count(actions[a]));
+
+	var actions_queue = [];
+	var clonedState;
+	var first = false;
+
+	if (k <= 0 && a <= 0) { // Starting from scratch.
+		clonedState =@ baseState;
+	}
+	else {
+		first = true;
+		actions_queue =@ uaTake(a, actions);
+
+		if (k > 0) { // Avoid empty sequence.
+			actions_queue += [uaTake(k, actions[a])];
+		}
+		else {
+			a -= 1; // Since the sequence was empty, we have to go back to the previous entity sequence.
+		}
+		clonedState =@ getLastState(actions_queue);
+		__obstacles =@ getLastObstacles(actions_queue);
+	}
+	if (getSelf(clonedState) === null) { return []; } // We died, no point continuing.
+
+	while (true) {
+		var mutated_actions;
+		if (first) {
+			mutated_actions =@ (actions_queue[a]);
+			first = false;
+		}
+		else {
+			mutated_actions =@ (actions_queue[a] = []);
+		}
+
+		var finished = true;
+		do {
+			finished = false;
+
+			var action;
+			var gameTuple = uctuple3([], clonedState, __obstacles);
+			gameTuple(action, clonedState, __obstacles);
+
 			opin_move += getOperations();
 			var list =@ getMove(clonedState);
-			list += list; // I want more chance to move.
+			list += list; // I want more chances to move.
 			opout_move += getOperations();
 			opin_attack += getOperations();
 			list += getAction(clonedState);
 			opout_attack += getOperations();
-			//if (list === []) { break; }
-			var curratedList =@ list; //aFilter(notEqual([]))(list);
+
+			var curratedList =@ list;
 			opin_select += getOperations();
 			do {
-				var actionGenerator =@ curratedList[randInt(0, count(curratedList) + 1)];
-				if (actionGenerator === null) { breakout =@ true; break; }
+				var i = randInt(0, count(curratedList) + 1);
+				var actionGenerator =@ curratedList[i];
+				if (actionGenerator === null) {
+					push(action, sayShit);
+					push(mutated_actions, gameTuple);
+					finished =@ true;
+					break;
+				}
 				var actionsGenerated =@ actionGenerator();
 				if (actionsGenerated !== []) {
-					actions += actionsGenerated[randInt(0, count(actionsGenerated))]();
+					var j = randInt(0, count(actionsGenerated));
+					action += actionsGenerated[j]();
+					push(mutated_actions, gameTuple);
 					break ;
 				}
 			} while (true);
 			opout_select += getOperations();
-			if (breakout) { break ; }
-		}
-		clonedState[S_SELF] = clonedState[S_ORDER][getSelf(clonedState)[ORDER]];
-	} while (getSelf(clonedState)[SUMMON]);
-	return ret;
+		} while (!finished);
+
+		var selfID = clonedState[S_ORDER][getSelf(clonedState)[ORDER]];
+
+		if (!(clonedState[S_ALL][selfID][SUMMON])) { break; }
+		clonedState = clonedState;
+		clonedState[S_SELF] = selfID;
+
+		a++;
+	}
+
+	return aFilter(isNonEmpty)(actions_queue);
+}
+
+function getLastAction(@gs) {
+	return getGameAction(getLastGameTuple(gs));
+}
+
+function getGameAction(@g) {
+	var a;
+	g(a, 0, 0);
+	return a;
+}
+
+function getLastState(@gs) {
+	return getGameState(getLastGameTuple(gs));
+}
+
+function getGameState(@g) {
+	var s;
+	g(0, s, 0);
+	return s;
+}
+
+function getLastObstacles(@gs) {
+	return getGameObstacles(getLastGameTuple(gs));
+}
+
+function getGameObstacles(@g) {
+	var o;
+	g(0, 0, o);
+	return o;
+}
+
+function getLastGameTuple(@gss) {
+	var gs =@ gss[count(gss) - 1];
+	return gs[count(gs) - 1];
 }
 
 function sumState(@all, @ls, @stat) {
@@ -192,8 +282,10 @@ function evaluateState(o) {
 	var ninfDiv = defaultDiv(log(0));
 	return /*memo1*/(function(@state) {
 		var xs =@ state[S_ALL];
+		if (xs[oID] === null) { return -log(0); } // We died :(
 		var dmgOnSelf = averageDmgFromLeeksOnCell(access(ENEMY))(xs[oID][POS])(xs);
 		xs[oID][HP] -= dmgOnSelf;
+		removeDead(state)(xs[oID]);
 
 		aIter(compose(removeDead(state))(applyEffects))(xs);
 
@@ -223,7 +315,7 @@ function evaluateState(o) {
 		xaminHPR = 1 - xaminHPR;
 		xaminHPR /= 13;
 		xaminHPR += 12 / 13;
-		var xSelfHPR = xs[oID] === null ? -log(0) : xs[oID][THP] / min(xs[oID][THP], 1 * xs[oID][HP]);
+		var xSelfHPR = xs[oID] === null ? -log(0) : xs[oID][THP] / min(xs[oID][THP], 0.90 * xs[oID][HP] + (-00));
 
 		var xeDist = 1;
 		var xaDist = 1;
@@ -280,7 +372,6 @@ function main() {
 	var population = pqEmpty();
 	var popInsert = pqInsert(population);
 	var popSelect = pqPop(population);
-	var existing = [];
 
 	opin_move = 0;
 	opout_move = 0;
@@ -292,16 +383,51 @@ function main() {
 	var opin_prio = 0, opout_prio = 0;
 
 	var maxOp = 18500000;
-	while (getOperations() < maxOp) {
+	while (getOperations() < maxOp && count(population) < 30) {
 		__obstacles = baseObstacles;
-		var actions =@ getActions(gameState);
+		var actions =@ mutateActions([[]], gameState);
+		if (actions === []) { continue; }
 		opin_evaluate += getOperations();
-		var value = evaluate(actions["state"]);
+		var value = evaluate(getLastState(actions));
 		opout_evaluate += getOperations();
 		totalPop++;
 		opin_prio += getOperations();
 		popInsert(value, actions);
 		opout_prio += getOperations();
+	}
+
+	while (getOperations() < maxOp) {
+		population =@ pqTake(population)(10);
+		var i = 0;
+		if (getFightContext() === FIGHT_CONTEXT_BATTLE_ROYALE) {
+		var ancestors = [];
+		ancestors += population;
+		while (getOperations() < maxOp && i < 10) {
+			__obstacles = baseObstacles;
+			var actions =@ mutateActions(ancestors[i](null), gameState);
+			i++;
+			if (actions === []) { continue; }
+			opin_evaluate += getOperations();
+			var value = evaluate(getLastState(actions));
+			opout_evaluate += getOperations();
+			totalPop++;
+			opin_prio += getOperations();
+			popInsert(value, actions);
+			opout_prio += getOperations();
+		}
+		}
+		while (getOperations() < maxOp && count(population) < 30) {
+			__obstacles = baseObstacles;
+			var actions =@ mutateActions([[]], gameState);
+			if (actions === []) { continue; }
+			opin_evaluate += getOperations();
+			var value = evaluate(getLastState(actions));
+			opout_evaluate += getOperations();
+			totalPop++;
+			opin_prio += getOperations();
+			popInsert(value, actions);
+			opout_prio += getOperations();
+		}
 	}
 		debugC((opout_move - opin_move) * 100 / OPERATIONS_LIMIT, COLOR_GREEN);
 		debugC((opout_attack - opin_attack) * 100 / OPERATIONS_LIMIT, COLOR_RED);
@@ -313,13 +439,13 @@ function main() {
 		debug('');
 
 	var value;
-	var elected = population[0](value);
+	var elected =@ population[0](value);
 	debug('value = ' + value);
-	debugC('action count: ' + count(elected['actions'][0]), BEST_COLOR);
-
-	ACTIONS_QUEUE =@ elected['actions'];
-	aIter(yield)(shift(ACTIONS_QUEUE));
-	sayShit();
+	debugC('action count: ' + count(elected[0]), BEST_COLOR);
+	//debugC(elected, COLOR_BLUE);
+	ACTIONS_QUEUE =@ elected;
+	aIter(act)(shift(ACTIONS_QUEUE));
+	//sayShit();
 }
 
 debug(getOperations() + 'op');
